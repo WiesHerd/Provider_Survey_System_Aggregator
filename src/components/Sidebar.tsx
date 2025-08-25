189b3f4472ccc0ad3bcc3ab012335d7c15be613c
WiesHerd <wherdzik@gmail.com>
2025-08-24 17:50:55 -0700

    Fix favicon caching issue - Update cache-busting parameters to v=3 to force browser refresh of purple BenchPoint logo

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -122,7 +122,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, setIsOpen }) => {
         <div className="flex items-center">
           <div className="w-12 h-12 flex items-center justify-center">
                          <img 
-               src={process.env.PUBLIC_URL + '/benchpoint-icon.svg?v=2'} 
+               src={process.env.PUBLIC_URL + '/benchpoint-icon.svg?v=3'} 
                alt="BenchPoint - Survey Aggregator" 
                className="w-12 h-12 object-contain" 
               onError={(e) => {
